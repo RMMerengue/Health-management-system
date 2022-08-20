d70ed3ff848a24bd52e476592df4399aeec46aa4
RMMerengue <1017703403@qq.com>
2022-08-21 03:59:45 +0800

    检查项分页

--- health_parent/health_service_provider/src/main/java/com/itheima/dao/CheckItemDao.java
+++ health_parent/health_service_provider/src/main/java/com/itheima/dao/CheckItemDao.java
@@ -1,7 +1,9 @@
 package com.itheima.dao;
 
+import com.github.pagehelper.Page;
 import com.itheima.pojo.CheckItem;
 
 public interface CheckItemDao {
     public void add(CheckItem checkItem);
+    public Page<CheckItem> selectByCondition(String queryString);
 }
--- health_parent/health_service_provider/src/main/java/com/itheima/service/impl/CheckItemServiceImpl.java
+++ health_parent/health_service_provider/src/main/java/com/itheima/service/impl/CheckItemServiceImpl.java
@@ -1,11 +1,18 @@
 package com.itheima.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.itheima.dao.CheckItemDao;
+import com.itheima.entity.PageResult;
+import com.itheima.entity.QueryPageBean;
 import com.itheima.pojo.CheckItem;
 import com.itheima.service.CheckItemService;
+import org.apache.zookeeper.Op;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service(interfaceClass = CheckItemService.class)
 @Transactional
@@ -16,5 +23,18 @@ public class CheckItemServiceImpl implements CheckItemService {
 
     public void add(CheckItem checkItem) {
         checkItemDao.add(checkItem);
+    }
+
+    @Override
+    public PageResult pageQuery(QueryPageBean queryPageBean) {
+        Integer currentPage = queryPageBean.getCurrentPage();
+        Integer pageSize = queryPageBean.getPageSize();
+        String queryString = queryPageBean.getQueryString();
+
+        PageHelper.startPage(currentPage,pageSize);
+        Page<CheckItem> page= checkItemDao.selectByCondition(queryString);
+        long total = page.getTotal();
+        List<CheckItem> rows = page.getResult();
+        return new PageResult(total,rows);
     }
 }

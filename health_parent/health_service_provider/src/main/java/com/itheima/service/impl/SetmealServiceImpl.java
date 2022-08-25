f63f32a3ef3def1d908720dbf4ee396b593a6ad7
RMMerengue <1017703403@qq.com>
2022-08-25 23:17:22 +0800

    使用Freemarker实现页面静态化

--- health_parent/health_service_provider/src/main/java/com/itheima/service/impl/SetmealServiceImpl.java
+++ health_parent/health_service_provider/src/main/java/com/itheima/service/impl/SetmealServiceImpl.java
@@ -9,10 +9,15 @@ import com.itheima.entity.PageResult;
 import com.itheima.entity.QueryPageBean;
 import com.itheima.pojo.Setmeal;
 import com.itheima.service.SetmealService;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import redis.clients.jedis.JedisPool;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,10 +27,16 @@ import java.util.Map;
 public class SetmealServiceImpl implements SetmealService {
 
     @Autowired
+    private FreeMarkerConfigurer freeMarkerConfigurer;
+
+    @Autowired
     private JedisPool jedisPool;
 
     @Autowired
     private SetmealDao setmealDao;
+
+    @Value("${out_put_path}")//从属性文件读取输出目录的路径
+    private String outputpath ;
 
     public void add(Setmeal setmeal, Integer[] checkgroupIds) {
         setmealDao.add(setmeal);
@@ -34,6 +45,58 @@ public class SetmealServiceImpl implements SetmealService {
 
         String fileName = setmeal.getImg();
         jedisPool.getResource().sadd(RedisConstant.SETMEAL_PIC_DB_RESOURCES,fileName);
+    }
+
+    //生成静态页面
+    public void generateMobileStaticHtml() {
+        //准备模板文件中所需的数据
+        List<Setmeal> setmealList = this.findAll();
+        //生成套餐列表静态页面
+        generateMobileSetmealListHtml(setmealList);
+        //生成套餐详情静态页面（多个）
+        generateMobileSetmealDetailHtml(setmealList);
+    }
+
+    //生成套餐列表静态页面
+    public void generateMobileSetmealListHtml(List<Setmeal> setmealList) {
+        Map<String, Object> dataMap = new HashMap<String, Object>();
+        dataMap.put("setmealList", setmealList);
+        this.generateHtml("mobile_setmeal.ftl","m_setmeal.html",dataMap);
+    }
+
+    //生成套餐详情静态页面（多个）
+    public void generateMobileSetmealDetailHtml(List<Setmeal> setmealList) {
+        for (Setmeal setmeal : setmealList) {
+            Map<String, Object> dataMap = new HashMap<String, Object>();
+            dataMap.put("setmeal", this.findById(setmeal.getId()));
+            this.generateHtml("mobile_setmeal_detail.ftl",
+                    "setmeal_detail_"+setmeal.getId()+".html",
+                    dataMap);
+        }
+    }
+
+    public void generateHtml(String templateName,String htmlPageName,Map<String, Object> dataMap){
+        Configuration configuration = freeMarkerConfigurer.getConfiguration();
+        Writer out = null;
+        try {
+            // 加载模版文件
+            Template template = configuration.getTemplate(templateName);
+            // 生成数据
+            File docFile = new File(outputpath + "\\" + htmlPageName);
+            out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(docFile)));
+            // 输出文件
+            template.process(dataMap, out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != out) {
+                    out.flush();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
     }
 
     public void setSetmealAndCheckgroup(Integer setmealId, Integer[] checkgroupIds){

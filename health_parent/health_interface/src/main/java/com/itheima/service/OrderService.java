e66562d577ff622d5c4e8977a27f30549a33b2e0
RMMerengue <1017703403@qq.com>
2022-08-26 01:30:41 +0800

    体检预约

---
+++ health_parent/health_interface/src/main/java/com/itheima/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.itheima.service;
+
+import com.itheima.entity.Result;
+import java.util.Map;
+/**
+ * 体检预约服务接口
+ */
+public interface OrderService {
+    //体检预约
+    public Result order(Map map) throws Exception;
+    public Map findById(Integer id) throws Exception;
+}
